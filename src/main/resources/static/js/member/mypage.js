6f7e775f7e4a6ab02b7ff71785f149ed7c18cb86
cherrycoder9 <cherrycoder9@gmail.com>
2024-07-18 17:42:35 +0900

    7교시 수업 종료

--- src/main/resources/static/js/member/mypage.js
+++ src/main/resources/static/js/member/mypage.js
@@ -4,6 +4,7 @@ mypage();
 
 function mypage() {
     console.log("mypage()");
+    let no = document.querySelector('#no');
     let id = document.querySelector('#id');
     let name = document.querySelector('#name');
     let email = document.querySelector('#email');
@@ -13,6 +14,8 @@ function mypage() {
         method: "get",
         url: "/member/myinfo",
         success: function (data) {
+            console.log(data);
+            no.innerHTML = data.no;
             id.innerHTML = data.id;
             name.innerHTML = data.name;
             email.innerHTML = data.email;
